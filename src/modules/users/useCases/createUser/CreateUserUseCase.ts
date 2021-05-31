dba3aa27a60c127a417d67c5a5bac83986541e36
Alexandre Marinho de Souza Júnior <alexandre.masj@gmail.com>
2021-05-30 22:09:01 -0300

    Concluded all tests

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,17 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userWithSameEmailAlreadyExists = this.usersRepository.findByEmail(
+      email
+    );
+
+    if (userWithSameEmailAlreadyExists) {
+      throw new Error("This email already exists");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (user === undefined || user.admin === false) {
+      throw new Error("User does not exists or is not an administrator");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
